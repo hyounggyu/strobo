96fb12cf94765dca7ac1a8f3ef5ed8eb3a307877
Hyounggyu Kim <khg@gist.ac.kr>
2013-10-18 12:45:41 +0900

    Install NI Vision
    1. Choose a NI ImageViewer tool item
    2. Change target framework
    3. Add NI references automatically
    4. Change Form1 design

--- strobo/Form1.Designer.cs
+++ strobo/Form1.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.imageViewer1 = new NationalInstruments.Vision.WindowsForms.ImageViewer();
+            this.SuspendLayout();
+            // 
+            // imageViewer1
+            // 
+            this.imageViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageViewer1.Location = new System.Drawing.Point(12, 12);
+            this.imageViewer1.Name = "imageViewer1";
+            this.imageViewer1.Size = new System.Drawing.Size(640, 480);
+            this.imageViewer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 562);
+            this.Controls.Add(this.imageViewer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private NationalInstruments.Vision.WindowsForms.ImageViewer imageViewer1;
     }
 }
 
