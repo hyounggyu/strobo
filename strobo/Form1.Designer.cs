43c4d559b220e8334443434792c3ac4929b602ac
Hyounggyu Kim <khg@gist.ac.kr>
2013-10-18 13:13:41 +0900

    Add textboxes and buttons

--- strobo/Form1.Designer.cs
+++ strobo/Form1.Designer.cs
@@ -29,6 +29,16 @@
         private void InitializeComponent()
         {
             this.imageViewer1 = new NationalInstruments.Vision.WindowsForms.ImageViewer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.interfaceTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bufNumTextBox = new System.Windows.Forms.TextBox();
+            this.quitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // imageViewer1
@@ -39,21 +49,122 @@
             this.imageViewer1.Size = new System.Drawing.Size(640, 480);
             this.imageViewer1.TabIndex = 0;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 499);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Interface Name";
+            // 
+            // interfaceTextBox
+            // 
+            this.interfaceTextBox.Location = new System.Drawing.Point(16, 516);
+            this.interfaceTextBox.Name = "interfaceTextBox";
+            this.interfaceTextBox.Size = new System.Drawing.Size(80, 20);
+            this.interfaceTextBox.TabIndex = 2;
+            this.interfaceTextBox.Text = "img0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 500);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "# of Images";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(115, 516);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(201, 500);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(201, 529);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 6;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(283, 499);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Current Image Number";
+            // 
+            // bufNumTextBox
+            // 
+            this.bufNumTextBox.Location = new System.Drawing.Point(286, 516);
+            this.bufNumTextBox.Name = "bufNumTextBox";
+            this.bufNumTextBox.Size = new System.Drawing.Size(100, 20);
+            this.bufNumTextBox.TabIndex = 8;
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(577, 500);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(75, 23);
+            this.quitButton.TabIndex = 9;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(664, 562);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.bufNumTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.interfaceTextBox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.imageViewer1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private NationalInstruments.Vision.WindowsForms.ImageViewer imageViewer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox interfaceTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox bufNumTextBox;
+        private System.Windows.Forms.Button quitButton;
     }
 }
 
