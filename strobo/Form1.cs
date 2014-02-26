f091161a6327ea54dba750daf69f9eacb26742f0
Hyounggyu Kim <khg@gist.ac.kr>
2014-02-26 10:51:18 +0900

    Add a renderWorker thread

--- strobo/Form1.cs
+++ strobo/Form1.cs
@@ -1,43 +1,44 @@
-﻿#define VOLTAGE
+﻿#define RANDOM
 
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+
 using NationalInstruments.Vision;
 using NationalInstruments.Vision.Acquisition.Imaq;
 using NationalInstruments.DAQmx;
-using NetMQ;
 
 
 namespace strobo
 {
     public partial class Form1 : Form
     {
+        private System.ComponentModel.BackgroundWorker acquisitionWorker, renderWorker;
+        ConcurrentQueue<PixelValue2D> volumeQueue;
+
+        // IMAQ variables
         private ImaqSession _session = null;
         private ImaqBufferCollection bufList;
         private VisionImage displayImage;
-        private System.ComponentModel.BackgroundWorker acquisitionWorker;
-        
+
+        // DAQ variables
         private AnalogMultiChannelReader analogInReader;
         private Task myTask;
-
         private string physicalChannelText;
         private double minimumValue;
         private double maximumValue;
         private double rateValue;
         private int    samplesPerChannelValue;
 
+        // Logging
         private StreamWriter sw;
-
-        // NetMQ
-        private NetMQContext context;
-        private NetMQSocket publisher;
 
         private struct UIUpdateArgs
         {
@@ -67,17 +68,20 @@ namespace strobo
             acquisitionWorker.ProgressChanged += new ProgressChangedEventHandler(acquisitionWorker_ProgressChanged);
             acquisitionWorker.WorkerReportsProgress = true;
             acquisitionWorker.WorkerSupportsCancellation = true;
+
+            renderWorker = new System.ComponentModel.BackgroundWorker();
+            renderWorker.DoWork += new DoWorkEventHandler(renderWorker_DoWork);
+            renderWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(renderWorker_RunWorkerCompleted);
+            renderWorker.ProgressChanged += new ProgressChangedEventHandler(renderWorker_ProgressChanged);
+            renderWorker.WorkerReportsProgress = true;
+            renderWorker.WorkerSupportsCancellation = true;
         }
 
         private void startButton_Click(object sender, EventArgs e)
         {
+#if DAQTEST
             sw = new StreamWriter( new FileStream("output.dat", FileMode.Create) );
-
-            // NetMQ
-            context = NetMQContext.Create();
-            publisher = context.CreatePublisherSocket();
-            publisher.Bind("tcp://*:5563");
-
+#endif
             try
             {
                 //  Update the UI.
@@ -85,7 +89,7 @@ namespace strobo
                 stopButton.Enabled = true;
                 bufNumTextBox.Text = "";
                 //pixelValTextBox.Text = "";
-                
+#if DAQ
                 //// DAQmx START
                 // Create a new task
                 myTask = new Task();
@@ -94,7 +98,7 @@ namespace strobo
                 maximumValue = 10.00;
                 rateValue = 10000.00;
                 samplesPerChannelValue = 1000;
-                
+
                 // Create a virtual channel
                 myTask.AIChannels.CreateVoltageChannel(physicalChannelText, "",
                     (AITerminalConfiguration)(-1), Convert.ToDouble(minimumValue),
@@ -105,7 +109,8 @@ namespace strobo
                 // Verify the Task
                 myTask.Control(TaskAction.Verify);
                 //// DAQmx END
-
+#endif
+#if IMAQ
                 //  Create a session.
                 _session = new ImaqSession(interfaceTextBox.Text);
 
@@ -125,7 +130,7 @@ namespace strobo
                 //  Configure and start the acquisition.
                 _session.Acquisition.Configure(bufList);
                 _session.Acquisition.AcquireAsync();
-
+#endif
                 //  Start the background worker thread.
                 acquisitionWorker.RunWorkerAsync(subCheckBox);
             }
@@ -143,10 +148,10 @@ namespace strobo
             //  Perform image processing here instead of the UI thread to avoid a
             //  sluggish or unresponsive UI.
             BackgroundWorker worker = (BackgroundWorker)sender;
-            bool flagSendMore = false;
             byte[] zero = new byte[] { 0 };
             try
             {
+#if IMAQ
                 PixelValue2D subPixels = new PixelValue2D(new byte[480, 640]);
                 uint imgnum = 0;
                 int tmp;
@@ -156,14 +161,11 @@ namespace strobo
 
                 PixelValue2D prePixels = _session.Acquisition.Extract(bufferNumber, out bufferNumber).ToPixelArray();
                 bufferNumber++;
-                //// DAQmx START
+#endif
+#if DAQ
                 double preVoltage = analogInReader.ReadSingleSample()[0];
-                //// DAQmx END
-
-                //// DAQmx START
                 double curVoltage;
-                //// DAQmx END
-
+#endif
                 //  Loop until we tell the thread to cancel or we get an error.  When this
                 //  function completes the acquisitionWorker_RunWorkerCompleted method will
                 //  be called.
@@ -173,12 +175,13 @@ namespace strobo
                     //  Extracting an image is a 0-copy operation, and we need to copy the
                     //  image here for display purposes only.  You can perform image processing
                     //  on the extractedImage without having to copy it.
+#if IMAQ
                     PixelValue2D curPixels = _session.Acquisition.Extract(bufferNumber, out outbufferNumber).ToPixelArray();
-
-                    //// DAQmx START
+#endif
+#if DAQ
                     curVoltage = analogInReader.ReadSingleSample()[0];
-                    //// DAQmx END
-
+#endif
+#if IMAQ
                     for (int x = 0; x < 640; x++)
                     {
                         for (int y=0; y < 480; y++)
@@ -197,17 +200,13 @@ namespace strobo
                     {
                         imageViewer.Image.ArrayToImage(curPixels);
                     }
-                    // Save Acqed bufferNumber
-                    sw.WriteLine("{0} {1}", outbufferNumber, curVoltage);
                     // Send image
                     // byte[,] 타입을 byte[]로 변경 -> 이거 안 해도 되어야함.
                     byte[] dest = new byte[subPixels.U8.Length];
                     Buffer.BlockCopy(subPixels.U8, 0, dest, 0, subPixels.U8.Length);
-                    //publisher.Send(dest);
-                    //double dvolt = (volt[voltIndex + 1] - volt[voltIndex - 1]) / 2.0;
+#endif
+#if DAQ
                     double dvolt = curVoltage - preVoltage;
-                    ////// 여기!!!! 속도!!!
-#if VOLTAGE
                     if (dvolt > 0.02)
                     {
                         publisher.SendMore(dest);
@@ -220,25 +219,24 @@ namespace strobo
                         // finish
                         flagSendMore = false;
                     }
-#else
-                    if (imgnum % 50  == 0)
-                    {
-                        publisher.Send(zero);
-                    }
-                    else
-                    {
-                        publisher.SendMore(dest);
-                    }
-                    imgnum++;
-#endif
                     preVoltage = curVoltage;
+#endif
+#if RANDOM
+                    // 여기에 Queue를 넣어야
+
+#endif
                     //  Update the UI by calling ReportProgress on the background worker.
                     //  This will call the acquisition_ProgressChanged method in the UI
                     //  thread, where it is safe to update UI elements.  Do not update UI
                     //  elements directly in this thread as doing so could result in a
                     //  deadlock.
+#if DAQ
                     worker.ReportProgress(0, new UIUpdateArgs(outbufferNumber, dvolt));
                     bufferNumber++;
+#elif IMAQ
+                    worker.ReportProgress(0, new UIUpdateArgs(outbufferNumber, 0));
+                    bufferNumber++;
+#endif
                 }
             }
             catch (ImaqException ex)
@@ -269,6 +267,21 @@ namespace strobo
                 MessageBox.Show(((ImaqException)e.Result).ToString(), "NI-IMAQ Error");
             }
             Cleanup();
+        }
+
+        void renderWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            return;
+        }
+
+        void renderWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            return;
+        }
+
+        void renderWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            return;
         }
 
         private void stopButton_Click(object sender, EventArgs e)
@@ -304,13 +317,13 @@ namespace strobo
                 _session.Close();
                 _session = null;
             }
+#if DAQTEST
             sw.Close();
-            // NetMQ
-            publisher.Close();
-            context.Dispose();
+#endif
             //  Update the UI.
             startButton.Enabled = true;
             stopButton.Enabled = false;
         }
+
     }
 }
