1b462175075210011b8d4f30a1906e8aae4b9f20
Hyounggyu Kim <khg@gist.ac.kr>
2014-02-28 13:49:19 +0900

    Remove comments

--- volrender/volrender_kernel.cu
+++ volrender/volrender_kernel.cu
@@ -234,12 +234,6 @@ void initCuda(void *h_volume, cudaExtent volumeSize)
 }
 
 extern "C"
-void updateVolume()
-{
-	checkCudaErrors(cudaMemcpy3D(&volumeParams));
-}
-
-extern "C"
 void freeCudaBuffers()
 {
     checkCudaErrors(cudaFreeArray(d_volumeArray));
@@ -261,95 +255,10 @@ void copyInvViewMatrix(float *invViewMatrix, size_t sizeofMatrix)
     checkCudaErrors(cudaMemcpyToSymbol(c_invViewMatrix, invViewMatrix, sizeofMatrix));
 }
 
-// TODO:
-
-const int iDivUp(int a, int b)
+extern "C"
+void updateVolume()
 {
-    return (a % b != 0) ? (a / b + 1) : (a / b);
+	checkCudaErrors(cudaMemcpy3D(&volumeParams));
 }
-
-
-/*
-typedef unsigned int uint;
-const int VOL_WIDTH = 640;
-const int VOL_HEIGHT = 480;
-const int VOL_DEPTH = 300;
-
-
-	// Parameters
-	m_density = 0.5f;
-	m_brightness = 5.0f;
-	m_transferOffset = 0.02f;
-	m_transferScale = 1.0f;
-	m_linearFiltering = true;
-	m_viewRotation = make_float3(-70.0, 0.0, 0.0);
-	m_viewTranslation = make_float3(0.0, 0.0, -5.0f);
-
-*/
-
-// 스타트 버튼을 누르고나서
-
-// Init CUDA
-// Alloc volume
-
-// initCuda(m_h_volume, make_cudaExtent(VOL_WIDTH, VOL_HEIGHT, VOL_DEPTH));
-// unsigned char *h_output = (unsigned char *)malloc(width*height*4);
-
-// Set gridsize and blocksize
-//	m_width = (uint)cx;
-//	m_height = (uint)cy;
-//	m_blockSize = dim3(16,16);
-//	m_gridSize = dim3(iDivUp(m_width, m_blockSize.x), iDivUp(m_height, m_blockSize.y));
-
-//	m_h_volume = calloc(VOL_WIDTH*VOL_HEIGHT*VOL_DEPTH, sizeof(unsigned char));
-
-// strobo 실행중
-
-// Set ViewMatrix
-/*
-	GLfloat modelView[16];
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
-	glLoadIdentity();
-	glRotatef(-m_viewRotation.x, 1.0, 0.0, 0.0);
-	glRotatef(-m_viewRotation.y, 0.0, 1.0, 0.0);
-	glTranslatef(-m_viewTranslation.x, -m_viewTranslation.y, -m_viewTranslation.z);
-	glGetFloatv(GL_MODELVIEW_MATRIX, modelView);
-	glPopMatrix();
-
-	m_invViewMatrix[0] = modelView[0];
-	m_invViewMatrix[1] = modelView[4];
-	m_invViewMatrix[2] = modelView[8];
-	m_invViewMatrix[3] = modelView[12];
-	m_invViewMatrix[4] = modelView[1];
-	m_invViewMatrix[5] = modelView[5];
-	m_invViewMatrix[6] = modelView[9];
-	m_invViewMatrix[7] = modelView[13];
-	m_invViewMatrix[8] = modelView[2];
-	m_invViewMatrix[9] = modelView[6];
-	m_invViewMatrix[10] = modelView[10];
-	m_invViewMatrix[11] = modelView[14];
-
-	copyInvViewMatrix(m_invViewMatrix, sizeof(float4)*3);
-*/
-
-// Rendering
-
-/*
-	// clear image
-	checkCudaErrors(cudaMemset(d_output, 0, m_width*m_height*4));
-
-	// Reset volume
-	memset((void *)m_h_volume, 0, VOL_WIDTH*VOL_HEIGHT*VOL_DEPTH);
-
-	// Copy volume
-	something something
-
-	render_kernel(m_gridSize, m_blockSize, d_output, m_width, m_height, m_density, m_brightness, m_transferOffset, m_transferScale);
-
-	checkCudaErrors(cudaMemcpy(h_output, d_output, width*height*4, cudaMemcpyDeviceToHost));
-
-	getLastCudaError("kernel failed");
-*/
 
 #endif // #ifndef _VOLUMERENDER_KERNEL_CU_
